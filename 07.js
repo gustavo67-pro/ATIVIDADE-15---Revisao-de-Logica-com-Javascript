3eddcc2548b9dc230bf59a21108f5f4edd5b2775
gustavo67-pro <gomescostagustavo89@gmail.com>
2026-06-24 14:06:07 -0300

    Create 07.js

---
+++ 07.js
@@ -0,0 +1,6 @@
+let num = Number(prompt("inisra o valor desejado: "))
+let potencia = Number(prompt("inisra a potencia do numero: "))
+for(i=1;i<potencia;i++){
+    num *= num 
+}
+console.log("resultado: "+ num)
